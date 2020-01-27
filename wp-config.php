28c8970295e1d58e8dc44a23d186a62f9ab7c81e
betkibet <kibetnamanga@gmail.com>
2020-01-27 15:33:30 +0300

    Initial commit

--- wp-config.php
+++ wp-config.php
@@ -20,10 +20,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'shamba' );
+define( 'DB_NAME', 'freshfa1_shamba' );
 
 /** MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'freshfa1_bet' );
 
 /** MySQL database password */
 define( 'DB_PASSWORD', 'Kitalale@2010' );
